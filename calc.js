0cb6e6763d231832df3d7599ca5a5547284a2434
Ameer Zaita <ameerzaita9@gmail.com>
2020-04-11 15:06:44 -0500

    Add input function to add values to inputArray

--- calc.js
+++ calc.js
@@ -1,8 +1,46 @@
 const calculator = document.getElementById('calculator');
 const buttons = Array.from(document.getElementsByTagName('button'));
 
+let inputArray = [];
+
 function input() {
-    console.log(event.target.value);
+    let inputValue = event.target.value;
+    console.log(inputValue);
+
+    function isNumber(a) {
+        if (a != '/' && a != '*' && a != '-' && a != '+' && a != 'c') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    if (inputArray.length == 0 ) {
+        if (isNumber(inputValue)) {
+            inputArray.push(inputValue);
+        }
+    } else {
+        // Check if previous value is number
+        if (isNumber(inputArray[inputArray.length - 1])) {
+            if (isNumber(inputValue)) {
+                inputArray[inputArray.length - 1] += inputValue;
+            } else {
+                inputArray.push(inputValue);
+            }
+        
+        /* If previous is not a number, then either push 
+        it to the array or replace previous. */
+        } else {
+            if (isNumber(inputValue)) {
+                inputArray.push(inputValue);
+            } else {
+                inputArray[inputArray.length - 1] = inputValue;
+            }
+        }
+    }
+
+    console.log(inputArray);
+    return inputArray;
 }
 
 function doMath(...args) {
